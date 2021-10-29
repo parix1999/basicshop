da49055ea14571c703b92addcc5273813d9b3c70
parix1999 <parisottoedoardo@gmail.com>
2021-10-29 18:20:01 +0200

    sistemazione registrazione

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,24 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="address" 
+                                type="text" 
+                                class="form-control @error('email') is-invalid @enderror" 
+                                name="address" value="{{ old('address') }}" 
+                                required autocomplete="address">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
